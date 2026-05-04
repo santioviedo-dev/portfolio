64c149d27f6081eac3b13b550c24ac653f4a5132
santioviedo-dev <oviedos412@gmail.com>
2026-05-04 19:04:59 -0300

    fix: add static output for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  output: "static",
   integrations: [tailwind()],
 });
